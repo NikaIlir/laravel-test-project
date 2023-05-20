9a9ceff512fa072938e08e4a2e227a21114739af
Ilir <ilirnk@gmail.com>
2023-05-20 17:23:26 +0200

    Add seeders for packages

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        for ($i = 0; $i < 15; $i++) {
+            DB::table('packages')->insert([
+                'uuid' => Str::uuid(),
+                'name' => Str::random(10),
+                'limit' => random_int(3, 8)
+            ]);
+        }
     }
 }
